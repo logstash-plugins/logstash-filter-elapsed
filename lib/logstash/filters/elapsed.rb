2d59ee741daa1494a047c039beb94f403a9f88df
Matt Walters <matt.walters@electronifie.com>
2015-05-21 17:18:24 -0400

    adding an actual timestamp, instead of a string that happens to be named timestamp...

--- lib/logstash/filters/elapsed.rb
+++ lib/logstash/filters/elapsed.rb
@@ -90,6 +90,7 @@ class LogStash::Filters::Elapsed < LogStash::Filters::Base
   PREFIX = "elapsed."
   ELAPSED_FIELD = PREFIX + "time"
   TIMESTAMP_START_EVENT_FIELD = PREFIX + "timestamp_start"
+  TIMESTAMP_SINCE_EPOCH_START_EVENT_FIELD = PREFIX + "timestamp_start_ts"
   HOST_FIELD = "host"
 
   ELAPSED_TAG = "elapsed"
@@ -226,6 +227,7 @@ class LogStash::Filters::Elapsed < LogStash::Filters::Base
       error_event[@unique_id_field] = element.event[@unique_id_field]
       error_event[ELAPSED_FIELD] = element.age
       error_event[TIMESTAMP_START_EVENT_FIELD] = Time.parse(element.event[@start_timestamp])
+      error_event[TIMESTAMP_SINCE_EPOCH_START_EVENT_FIELD] = Time.parse(element.event[@start_timestamp]).to_f
 
       events << error_event
       filter_matched(error_event)
